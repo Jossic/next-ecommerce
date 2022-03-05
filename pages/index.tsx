5db6a2829d7bd4672051fde7b888e6804b8e907e
Jossic <jossic.lapierre@gmail.com>
2022-03-05 22:14:18 +0100

    get all products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
-import type { NextPage } from 'next';
+import type { InferGetStaticPropsType, NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import getAllProducts from '../framework/shopify/product/get-all-products';
 
-const Home: NextPage = () => {
-	return <div>Hello world</div>;
+const Home = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
+	return <div>{products}</div>;
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+	const products = await getAllProducts();
+
+	return {
+		props: {
+			products,
+		},
+	};
 };
 
 export default Home;
