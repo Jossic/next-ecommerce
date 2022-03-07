22c6a0781f42e2ac146f640d9302a6cc2dee66fd
Jossic <jossic.lapierre@gmail.com>
2022-03-07 10:42:44 +0100

    Normalize & return products array

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -6,7 +6,13 @@ type ReturnType = { products: ProductConnection };
 
 const getAllProducts = async (): Promise<any> => {
 	const { data } = await fetchAPI<ReturnType>({ query: getAllProductsQuery });
-	return data.products;
+
+	const products =
+		data.products.edges.map(({ node: product }) => {
+			return product;
+		}) ?? [];
+
+	return products;
 };
 
 export default getAllProducts;
