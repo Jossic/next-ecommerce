6c6743e34a71a351fdc7ed84ca602952b48d9563
Jossic <jossic.lapierre@gmail.com>
2022-03-08 10:03:21 +0100

    Adding custom style

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
+import style from './Layout.module.css';
 
 const Layout: FC = ({ children }) => {
-	return <div className='bg-red-800 text-white'>{children}</div>;
+	return <main className={style.root}>{children}</main>;
 };
 
 export default Layout;
