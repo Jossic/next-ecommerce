a624dbceee3f6d9610913e2c991641583cc46e46
Jossic <jossic.lapierre@gmail.com>
2022-03-08 11:54:58 +0100

    Adding Image to productCard

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,12 +1,38 @@
 import { Product } from '@common/types/product';
 import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
 	product: Product;
 }
 
+const placeholder = '/product-image-placeholder.svg';
+
 const ProductCard: FC<Props> = ({ product }) => {
-	return <div>{product.name}</div>;
+	return (
+		<Link href={`/products/${product.slug}`}>
+			<a>
+				<div>
+					<h3>
+						<span>{product.name}</span>
+					</h3>
+					<span>$4</span>
+				</div>
+				{product.images && (
+					<Image
+						width={540}
+						height={540}
+						// objectFit="cover"
+						quality='85'
+						layout='responsive'
+						src={placeholder}
+						alt={product.name ?? 'Product image'}
+					/>
+				)}
+			</a>
+		</Link>
+	);
 };
 
 export default ProductCard;
