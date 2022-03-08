6024ae99f2d6aab8114fc6188d420ca68d6dcc45
Jossic <jossic.lapierre@gmail.com>
2022-03-08 12:02:55 +0100

    Adding Grid Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Layout } from '@components/common';
 import { ProductCard } from '@components/product';
+import { Grid } from '@components/ui';
 import { getConfig } from '@framework/api/config';
 import getAllProducts from '@framework/product/get-all-products';
 import type { InferGetStaticPropsType, NextPage } from 'next';
@@ -9,11 +10,13 @@ export default function Home({
 	products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
 	return (
-		<div>
-			{products.slice(0, 3).map((product) => (
-				<ProductCard key={product.id} product={product} />
-			))}
-		</div>
+		<>
+			<Grid>
+				{products.slice(0, 3).map((product) => (
+					<ProductCard key={product.id} product={product} />
+				))}
+			</Grid>
+		</>
 	);
 }
 
