4ce0423601b5a551fc8198650a4373e5ad6b32ef
Jossic <jossic.lapierre@gmail.com>
2022-03-09 14:27:10 +0100

    Hero Flex

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -3,11 +3,11 @@ import s from './Container.module.css';
 
 interface Props {
 	children: ReactNode | ReactNode[];
-	element?: ComponentType<HTMLAttributes<HTMLElement>>;
+	tag?: ComponentType<HTMLAttributes<HTMLElement>>;
 }
 
-const Container: FC<Props> = ({ children, element: Component = 'div' }) => {
-	return <Component className={s.container}>{children}</Component>;
+const Container: FC<Props> = ({ children, tag: Wrapper = 'div' }) => {
+	return <Wrapper className={s.container}>{children}</Wrapper>;
 };
 
 export default Container;
