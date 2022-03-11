3ed8617f4b68c4e3a5fcef9bdb6e06771ba64e54
Jossic <jossic.lapierre@gmail.com>
2022-03-11 10:54:54 +0100

    Adding Marquee Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Layout } from '@components/common';
 import { ProductCard } from '@components/product';
-import { Grid, Hero } from '@components/ui';
+import { Grid, Hero, Marquee } from '@components/ui';
 import { getConfig } from '@framework/api/config';
 import getAllProducts from '@framework/product/get-all-products';
 import type { InferGetStaticPropsType, NextPage } from 'next';
@@ -20,6 +20,11 @@ export default function Home({
 				headline='Hello'
 				description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, tempora! Accusamus fugiat a dolor repellendus modi pariatur, consequuntur deleniti iste numquam consectetur. Quidem, nulla! Incidunt, maiores rerum! Aut voluptates sint, necessitatibus expedita blanditiis consequatur? Aut saepe expedita ad maiores. Nihil aliquam eaque cumque iure quis necessitatibus, sit officia culpa voluptatum!'
 			/>
+			<Marquee>
+				{products.slice(0, 3).map((product) => (
+					<ProductCard key={product.id} product={product} />
+				))}
+			</Marquee>
 		</>
 	);
 }
