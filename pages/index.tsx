b92c9f362f9ded2d7440dac7b94646a75212373f
Jossic <jossic.lapierre@gmail.com>
2022-03-11 11:57:54 +0100

    Layout B for second Grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,11 @@ export default function Home({
 					/>
 				))}
 			</Marquee>
+			<Grid layout='B'>
+				{products.slice(0, 3).map((product) => (
+					<ProductCard key={product.id} product={product} />
+				))}
+			</Grid>
 		</>
 	);
 }
