531f90db61bcbeb01563289101a3a3cc2c6fbcc2
Jossic <jossic.lapierre@gmail.com>
2022-03-11 13:04:25 +0100

    Marquee variant

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home({
 				headline='Hello'
 				description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, tempora! Accusamus fugiat a dolor repellendus modi pariatur, consequuntur deleniti iste numquam consectetur. Quidem, nulla! Incidunt, maiores rerum! Aut voluptates sint, necessitatibus expedita blanditiis consequatur? Aut saepe expedita ad maiores. Nihil aliquam eaque cumque iure quis necessitatibus, sit officia culpa voluptatum!'
 			/>
-			<Marquee>
+			<Marquee variant='secondary'>
 				{products.slice(0, 3).map((product) => (
 					<ProductCard
 						key={product.id}
@@ -34,6 +34,15 @@ export default function Home({
 					<ProductCard key={product.id} product={product} />
 				))}
 			</Grid>
+			<Marquee>
+				{products.slice(0, 3).map((product) => (
+					<ProductCard
+						key={product.id}
+						product={product}
+						variant='slim'
+					/>
+				))}
+			</Marquee>
 		</>
 	);
 }
